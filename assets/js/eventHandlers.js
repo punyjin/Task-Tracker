2d37814f6ed8e60f6b4974424da53dcf250d49b6
xorbit <91112547+punyjin@users.noreply.github.com>
2024-08-27 13:21:29 +0700

    Add files via upload

--- assets/js/eventHandlers.js
+++ assets/js/eventHandlers.js
@@ -1,5 +1,6 @@
 import { handleSave } from './handles/Save.js';
 import { closeModal, showDetails } from './events/modal_work.js';
 import { fetchData } from './events/fetchData.js';
+import { initializeCountdowns } from "./events/timer.js";
 
-export {handleSave, closeModal, showDetails, fetchData};
+export {handleSave, closeModal, showDetails, fetchData, initializeCountdowns};
